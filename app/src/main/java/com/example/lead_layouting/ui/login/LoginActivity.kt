4b3ae87d4d80b0eaac85bd4f0fd21028b75e2aea
ashenladd <akifrh24@gmail.com>
2023-05-29 10:02:30 +0700

    Adding validate

--- app/src/main/java/com/example/lead_layouting/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/lead_layouting/ui/login/LoginActivity.kt
@@ -2,11 +2,42 @@ package com.example.lead_layouting.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lead_layouting.R
+import android.text.Editable
+import android.util.Log
+import android.widget.Toast
+import com.example.lead_layouting.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setupClickListener()
+    }
+
+    private fun setupClickListener(){
+        binding.apply {
+            btnSignin.setOnClickListener{
+                var username = tietUsername.text.toString()
+                var password = tietPassword.text.toString()
+                Log.d("sample","sample: Username $username")
+
+                var valid = validate(username = username, password =password)
+                if(valid){
+                    Toast.makeText(this@LoginActivity,"$username anda berhasil login",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this@LoginActivity,"anda gagal login, silahkan coba lagi",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+    }
+    private fun validate(username: String, password:String):Boolean{
+        return username == "Admin" && password == "admin"
     }
 }
